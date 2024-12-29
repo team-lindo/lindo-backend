c9f9b60eb87d6ac42789b71d7853fc965714693c
BrianDYKim <18k7102dy@gmail.com>
2024-12-29 16:24:32 +0900

    fix: 공통 configuration에 EnableWebSecurity 어노테이션 추가

--- src/main/java/team/lindo/backend/presentation/config/app/LindoAppConfiguration.java
+++ src/main/java/team/lindo/backend/presentation/config/app/LindoAppConfiguration.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import team.lindo.backend.common.extensions.objectMapper.ObjectMapperExtension;
 
 @Configuration
+@EnableWebSecurity
 public class LindoAppConfiguration {
     @Bean
     @Primary
