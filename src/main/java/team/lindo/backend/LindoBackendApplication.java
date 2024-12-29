08d76960352a3ce7867614240f663545a1d451af
BrianDYKim <18k7102dy@gmail.com>
2024-12-29 15:09:59 +0900

    project init

---
+++ src/main/java/team/lindo/backend/LindoBackendApplication.java
@@ -0,0 +1,13 @@
+package team.lindo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LindoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LindoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/lindo/backend/LindoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package team.lindo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LindoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
