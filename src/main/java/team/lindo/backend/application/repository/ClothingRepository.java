4eb33766e4e503400a713fc7f33358203acdd56e
whddnjs33 <whddnjs039@naver.com>
2025-02-05 20:27:52 +0900

    옷장 api

--- src/main/java/team/lindo/backend/application/repository/ClothingRepository.java
+++ src/main/java/team/lindo/backend/application/repository/ClothingRepository.java
@@ -1,9 +1,11 @@
 package team.lindo.backend.application.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import team.lindo.backend.application.entity.Clothing;
 import java.util.List;
 
+@Repository
 public interface ClothingRepository extends JpaRepository<Clothing, Long> {
     List<Clothing> findByCategory(Clothing.Category category);
 }
